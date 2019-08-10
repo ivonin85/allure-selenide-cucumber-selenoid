c8b8819b391f6d84bde27f1adc4d3d0d6e135643
ivonin85 <ivonin85@gmail.com>
2019-08-11 00:59:17 +0300

    ozon

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -18,14 +18,13 @@ import org.junit.runner.RunWith;
 public class TestRunner {
     @BeforeClass
     public static void before() {
-        WebDriverManager.chromedriver().version("76").setup();
-        //Configuration.baseUrl = "http://localhost:4444/";
-        //Configuration.remote = "http://18.130.227.197:4444/wd/hub";
+        //WebDriverManager.chromedriver().version("76").setup();
+        Configuration.baseUrl = "http://localhost:4444/";
+        Configuration.remote = "http://35.158.91.91:4444/wd/hub";
         Configuration.timeout = 20000;
-        // ChromeDriverManager.getInstance().setup();
-        //Configuration.browserSize = "1600x1024";
-        //Configuration.browserCapabilities.setCapability("enableVNC", true);
-        //Configuration.browserCapabilities.setCapability("enableVideo", false);
+        Configuration.browserSize = "1600x1024";
+        Configuration.browserCapabilities.setCapability("enableVNC", true);
+        Configuration.browserCapabilities.setCapability("enableVideo", false);
     }
 
 }
